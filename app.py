08b4c31398a3e1975e104befe4d2b6ddf6834999
Andrés Pedraza Granados <adpg.611@gmail.com>
2022-06-10 22:41:24 -0500

    quiero dormir

--- app.py
+++ app.py
@@ -123,8 +123,9 @@ def pedir_articulo():
         'cantidad': randint(int(request.form['cantidad']) * 0.8, int(request.form['cantidad']) * 1.2)
       }
     })
-    db.articulos.update_one({'_id': ObjectId(id)}, {"$inc": {
-      'cantidad': db.pedidos.find_one({'_id': ObjectId(id)})['entrega']['cantidad']
+    pedido = db.pedidos.find_one({'_id': ObjectId(id)})
+    db.articulos.update_one({'_id': ObjectId(pedido['id_articulo'])}, {"$inc": {
+      'cantidad': pedido['entrega']['cantidad']
     }})
     return jsonify({'_id': str(ObjectId(id))})
   else:
